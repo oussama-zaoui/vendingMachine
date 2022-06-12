2ae5556555cb3ca6617f334340988a4feef035fd
Oussama ZAOUI <karakoradoffy@gmail.com>
2022-06-12 12:47:43 +0100

    test acces database issue solved

--- src/test/java/com/oussama/vendingmachine/VendingmachineApplicationTests.java
+++ src/test/java/com/oussama/vendingmachine/VendingmachineApplicationTests.java
@@ -1,13 +1,24 @@
 package com.oussama.vendingmachine;
 
+import com.oussama.vendingmachine.services.UserService;
+import com.oussama.vendingmachine.utils.Constant;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @SpringBootTest
 class VendingmachineApplicationTests {
 
+	@Autowired
+	UserService userService;
+
 	@Test
-	void contextLoads() {
+	void deposit() {
+		System.out.println("this is a test");
+		System.out.println("this message is coming from teste "+userService.deposit(50));
+		assertEquals("", Constant.OK,userService.deposit(100));
 	}
 
 }

107c955593e6ac05efbe79c2a10bb367786ae7e7
Oussama ZAOUI <karakoradoffy@gmail.com>
2022-06-12 19:31:57 +0100

    test update

--- src/test/java/com/oussama/vendingmachine/VendingmachineApplicationTests.java
+++ src/test/java/com/oussama/vendingmachine/VendingmachineApplicationTests.java
@@ -4,20 +4,28 @@ import com.oussama.vendingmachine.services.UserService;
 import com.oussama.vendingmachine.utils.Constant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class VendingmachineApplicationTests {
 
 	@Autowired
 	UserService userService;
 
+	@Autowired
+	MockMvc mockMvc;
+
+
+
 	@Test
+	@WithMockUser(username = "karim",authorities = "seller")
 	void deposit() {
-		System.out.println("this is a test");
-		System.out.println("this message is coming from teste "+userService.deposit(50));
 		assertEquals("", Constant.OK,userService.deposit(100));
 	}
 
